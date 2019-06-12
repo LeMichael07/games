fbd7a8ec4ea110ced8d2316630341c82fea147ed
Mike Le <mike@Mikes-MacBook-Pro.local>
2019-06-11 20:56:29 -0400

    updated rock-paper-scissors

---
+++ rock-paper-scissors/class-example-rock-paper-scissors-main.js
@@ -0,0 +1,54 @@
+var content = document.getElementById('content');
+
+var winningConditions = {
+    "rock": { "paper": "Player Wins", "scissors": "Computer Wins", "rock": "Tie" },
+    "paper": { "scissors": "Player Wins", "rock": "Computer Wins", "paper": "Tie" },
+    "scissors": { "rock": "Player Wins", "paper": "Computer Wins", "scissors": "Tie" }
+}
+
+var rockPaperScissors = {
+    playerChoice: "",
+    computerChoice: "",
+    message: ""
+}
+
+// Math.floor(Math.random() * 3);
+
+function updateGameState(playerChoice) {
+    rockPaperScissors.playerChoice = playerChoice;
+    rockPaperScissors.computerChoice = generateComputerChoice();
+    rockPaperScissors.message = generateMessage();
+    renderGame(rockPaperScissors);
+}
+
+function generateComputerChoice() {
+    choices = ['rock', 'paper', 'scissors'];
+    randomNum = Math.floor(Math.random() * 3);
+    return choices[randomNum];
+}
+
+function generateMessage() {
+    let winCondition = winningConditions[rockPaperScissors.playerChoice];
+    return winCondition[rockPaperScissors.computerChoice];
+}
+
+renderGame(rockPaperScissors);
+
+function renderGame(game) {
+    // Change this render function to use the "game" parameter
+
+    content.innerHTML = `
+        <div class="container d-flex flex-column justify-content-start align-items-center">
+            <div class="w-50 text-center">
+                <button class="btn btn-primary" id="r" onClick="updateGameState('rock');">Rock</button>
+                <button class="btn btn-primary" id="p" onClick="updateGameState('paper');">Paper</button>
+                <button class="btn btn-primary" id="s" onClick="updateGameState('scissors');">Scissors</button>
+            </div>
+            <div class="d-flex justify-content-center">
+                <div class="m-5">You played: <b>${game.playerChoice}</b></div>
+                <div class="m-5">The computer played: <b>${game.computerChoice}</b></div>
+            </div>
+            <h1 class="text-center">${game.message}</h1>
+        </div>
+    `
+}
--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -1,28 +1,170 @@
-var content = document.getElementById('content');
-
-var rockPaperScissors = "Replace this with your own abstraction of Rock Paper Scissors"
-
-content.innerHTML = renderGame(rockPaperScissors);
-
-
+var content = document.getElementById("content");
+var game = {
+    player: "",
+    computer: ""
+}
 
 
-function renderGame(game) {
-    // Change this render function to use the "game" parameter
+// Event Listeners
+
+// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, 
+// without waiting for stylesheets, images, and subframes to finish loading.
+document.addEventListener("DOMContentLoaded", init(game));
+content.addEventListener("click", humanPlay);
+
+function init() {
+    content.innerHTML = renderGame(game);
+}
+
+
+
+// Your Turn
+
+function humanPlay(humanTurn) {
+    computerPlay(game);
+
+    var target = humanTurn.target
+    if (target.id === "rock") {
+        game.player = "Rock";
+
+    } else if (target.id === "paper") {
+        game.player = "Paper";
+
+    } else if (target.id === "scissor") {
+        game.player = "Scissor";
+
+    } else{
+        return;
+    }
+
+    // Check for Winner after human turn.
+    checkWinner();
+
+}
+
+
+
+// Computer Turn. 
+// Used Math.random() to randomize computer choices from switch statement.
+// Math.random() * (max - min) + min; This formula gets a random number between 2 values. 
+
+function computerPlay() {
+    var cpu = Math.round(Math.random() * (3 -1) +1);
+    switch (cpu) {
+        case 1:
+            game.computer = "Paper";
+            break;
+        case 2:
+            game.computer = "Scissor";
+            break;
+        case 3:
+            game.computer = "Rock";
+            break;
+        default:
+            break;
+    }
+}
+
+
+// Winning Conditions
+
+function checkWinner() {
+    //Condition for Rock
+    if (game.player === "Rock") {
+        if (game.computer === "Rock") {
+            renderDraw();
+            return;
+        }
+    
+        if (game.computer === "Paper"){
+            renderLose();
+            return;
+        }
+    
+        if (game.computer === "Scissor"){
+            renderWin();
+            return;
+        }
+    }
+
+    // Condition for Paper
+    if (game.player === "Paper") {
+        if (game.computer === "Rock") {
+            renderWin();
+            return;
+        }
+    
+        if (game.computer === "Paper"){
+            renderDraw();
+            return;
+        }
+
+        if (game.computer === "Scissor"){
+            renderLose();
+            return;
+        }
+    }
+
+    // Condition for Scissor
+    if (game.player === "Scissor") {
+        if (game.computer === "Rock") {
+            renderLose();
+            return;
+        }
+
+        if (game.computer === "Paper"){
+            renderWin();
+            return;
+        }
+
+        if (game.computer === "Scissor"){
+            renderDraw();
+            return;
+        }
+    }
+}
+
+
+
+//Rendering Functions
+
+function renderWin() {
+    renderResults();
+    document.getElementById("result-text").innerHTML = "Win";
+}
+
+function renderLose() {
+    renderResults();
+    document.getElementById("result-text").innerHTML = "Lose";
+}
+
+function renderDraw() {
+    renderResults();
+    document.getElementById("result-text").innerHTML = "Draw";
+}
+
+function renderResults (){
+    document.getElementById("human-play").innerHTML = `You played: <b> ${game.player} </b>`;
+    document.getElementById("computer-play").innerHTML = `Computer played: <b> ${game.computer} </b>`;
+}
+
+
+
+function renderGame() {
 
     return `
         <div class="container d-flex flex-column justify-content-start align-items-center">
             <h4>Choose your weapon:</h4>
             <div class="w-50 text-center">
-                <button class="btn btn-primary">Rock</button>
-                <button class="btn btn-primary">Paper</button>
-                <button class="btn btn-primary">Scissors</button>
+                <button class="btn btn-primary" id="rock">Rock</button>
+                <button class="btn btn-primary" id="paper">Paper</button>
+                <button class="btn btn-primary" id="scissor">Scissors</button>
             </div>
             <div class="d-flex justify-content-center">
-                <div class="m-5">You played: <b>ROCK</b></div>
-                <div class="m-5">The computer played: <b>SCISSORS</b></div>
+                <div class="m-5" id="human-play">You played: </div>
+                <div class="m-5" id="computer-play">The computer played: </div>
             </div>
-            <h1 class="text-center">You win!</h1>
+            <h1 class="text-center" id="result-text"></h1>
         </div>
     `
 }
